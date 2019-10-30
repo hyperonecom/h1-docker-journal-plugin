34f08f241952c9ed73b9e520999c274784de8348
Adam Dobrawy <naczelnik@jawnosc.tk>
2019-10-31 00:29:37 +0100

    Improve formatting of tests

--- tests/protocol.js
+++ tests/protocol.js
@@ -7,16 +7,18 @@ const test = require('ava');
 const header = Buffer.from('00000068', 'hex');
 const payload = Buffer.from('0a067374646f757410f5d196bdc9d3b4da151a542f7379732f6b65726e656c2f736c61622f3a74412d303030303230302f6367726f75702f766d5f617265615f73747275637428313838303a736d62642e73657276696365292f73616e6974795f636865636b730d', 'hex');
 
-test('parse docker stream', async t => new Promise((resolve, reject) => Readable.from([header, payload])
-    .pipe(new ParseDockerStream())
-    .on('data', msg => {
-        t.true(msg.source === 'stdout');
-        return resolve();
-    })
-    .on('error', reject)
+test('parse docker stream', t => new Promise((resolve, reject) =>
+    Readable
+        .from([header, payload])
+        .pipe(new ParseDockerStream())
+        .on('data', msg => {
+            t.true(msg.source === 'stdout');
+            return resolve();
+        })
+        .on('error', reject)
 ));
 
-test('parse journal stream', async t => new Promise((resolve, reject) => {
+test('parse journal stream', t => new Promise((resolve, reject) => {
     let count = 0;
 
     Readable.from([
@@ -26,7 +28,7 @@ test('parse journal stream', async t => new Promise((resolve, reject) => {
         .pipe(new ParseJournalStream())
         .on('data', msg => {
             t.true(msg.x === '25');
-            count+=1;
+            count += 1;
         })
         .on('error', reject)
         .on('finish', () => {
